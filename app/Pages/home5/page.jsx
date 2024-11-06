e34e4b94e64a003fb8cd3efcc74e9bb1366d89ec
jisan5620jk <jisank5620@gmail.com>
2024-11-06 10:58:09 +0600

    Add Component

--- app/Pages/home5/page.jsx
+++ app/Pages/home5/page.jsx
@@ -1,4 +1,5 @@
 import About from "@/app/Component5/About/About";
+import Analytics from "@/app/Component5/Analytics/Analytics";
 import Banner from "@/app/Component5/Banner/Banner";
 import Brand from "@/app/Component5/Brand/Brand";
 import Navbar5 from "@/app/Shared/Navbar/Navbar5";
@@ -10,6 +11,7 @@ const page = () => {
     <Banner />
     <Brand />
     <About />
+    <Analytics />
     </>
   );
 };
