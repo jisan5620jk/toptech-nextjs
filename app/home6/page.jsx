f1ba47fd88d6360fe85dcf0857751337337a5611
jisan5620jk <jisank5620@gmail.com>
2024-11-14 11:09:10 +0600

    Update Path Directory

--- app/home6/page.jsx
+++ app/home6/page.jsx
@@ -1,10 +1,10 @@
-import Navbar6 from '@/app/Shared/Navbar/Navbar6';
-import Banner from '../../Component6/Banner/Banner';
-import WhyChoose from '@/app/Component6/WhyChoose/WhyChoose';
-import Service from '@/app/Component6/Service/Service';
-import Experience from '@/app/Component6/Experience/Experience';
-import Perform from '@/app/Component6/Perform/Perform';
-import Footer6 from '@/app/Shared/Footer/Footer6';
+import Service from '../Component1/Service/Service';
+import WhyChoose from '../Component2/WhyChoose/WhyChoose';
+import Banner from '../Component6/Banner/Banner';
+import Experience from '../Component6/Experience/Experience';
+import Perform from '../Component7/Perform/Perform';
+import Footer6 from '../Shared/Footer/Footer6';
+import Navbar6 from '../Shared/Navbar/Navbar6';
 
 const page = () => {
   return (
--- app/home7/page.jsx
+++ app/home7/page.jsx
@@ -1,25 +1,25 @@
-import Footer7 from "@/app/Shared/Footer/Footer7";
-import AppScreen from "../../Component7/AppScreen/AppScreen";
-import Banner from "../../Component7/Banner/Banner";
-import Benifits from "../../Component7/Benifits/Benifits";
-import Blog from "../../Component7/Blog/Blog";
-import Brand from "../../Component7/Brand/Brand";
-import Collaboration from "../../Component7/Collaboration/Collaboration";
-import ContentSlider from "../../Component7/ContentSlider/ContentSlider";
-import Download from "../../Component7/Download/Download";
-import Faqs from "../../Component7/Faqs/Faqs";
-import Feature from "../../Component7/Feature/Feature";
-import Pricing from "../../Component7/Pricing/Pricing";
-import Process from "../../Component7/Process/Process";
-import Support from "../../Component7/Support/Support";
-import TeamMember from "../../Component7/TeamMember/TeamMember";
-import Testimonial from "../../Component7/Testimonial/Testimonial";
-import Navbar7 from "@/app/Shared/Navbar/Navbar7";
+import Banner from '../Component7/Banner/Banner';
+import Feature from '../Component7/Feature/Feature';
+import Collaboration from '../Component7/Collaboration/Collaboration';
+import Benifits from '../Component7/Benifits/Benifits';
+import Brand from '../Component7/Brand/Brand';
+import Process from '../Component7/Process/Process';
+import ContentSlider from '../Component7/ContentSlider/ContentSlider';
+import AppScreen from '../Component7/AppScreen/AppScreen';
+import Pricing from '../Component7/Pricing/Pricing';
+import Testimonial from '../Component7/Testimonial/Testimonial';
+import TeamMember from '../Component7/TeamMember/TeamMember';
+import Faqs from '../Component7/Faqs/Faqs';
+import Download from '../Component7/Download/Download';
+import Blog from '../Component7/Blog/Blog';
+import Support from '../Component7/Support/Support';
+import Navbar7 from '../Shared/Navbar/Navbar7';
+import Footer7 from '../Shared/Footer/Footer7';
 
 const page = () => {
   return (
     <>
-    <Navbar7 />
+      <Navbar7 />
       <Banner />
       <Feature />
       <Collaboration />
