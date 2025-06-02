c8ca2058bc077d3c2373a0ec43ffa9b62cbe4977
jisan5620jk <jisank5620@gmail.com>
2025-06-02 15:16:21 +0600

    update home six

--- app/home6/page.jsx
+++ app/home6/page.jsx
@@ -1,12 +1,12 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import Banner from "../Component6/Banner/Banner";
 import Experience from "../Component6/Experience/Experience";
 import Perform from "../Component6/Perform/Perform";
 import Service from "../Component6/Service/Service";
 import Footer6 from "../Shared/Footer/Footer6";
 import Navbar6 from "../Shared/Navbar/Navbar6";
-import dynamic from "next/dynamic";
 
 const WhyChoose = dynamic(() => import("../Component6/WhyChoose/WhyChoose"), {
   ssr: false,
